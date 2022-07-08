03036088c174e7956a468202e38ad29f96fff01d
“eldos87” <thomasv855@gmail.com>
2022-07-08 20:53:17 +0800

    configuation added

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -6,7 +6,7 @@ log_dir = "Housing_Logs"
 os.makedirs(log_dir, exist_ok=True)
 
 now = datetime.now().strftime("%d-%m-%Y %H-%M-%S")
-file_name = f"log_{now}"
+file_name = f"log_{now}.log"
 file_path = os.path.join(log_dir, file_name)
 file_format = '[%(asctime)s] %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(filename=file_path, filemode='w', format=file_format, level=logging.INFO)
